b4a6e1e7875eaedf9a2c5aa8aed98d6b46c16a12
Anvar Nazar <anvarnasar@ymail.com>
2019-12-28 18:25:39 +0530

    player and movement

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,8 +5,12 @@ import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
+	var _player:Player;
+	
 	override public function create():Void
 	{
+		_player = new Player(20, 20);
+		add(_player);
 		super.create();
 	}
 
